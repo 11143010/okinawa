1e0178a4cdf5a102d1fc8fb4cb9c0a2443232a6c
11143010 <11143010@gms.tcu.edu.tw>
2026-04-28 16:56:52 +0800

    Refactor app into Vue + Tailwind multi-file structure

---
+++ src/components/ui/Tabs.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({ tabs: Array, active: String })
+defineEmits(['change'])
+</script>
+
+<template>
+  <div class="flex overflow-x-auto rounded-xl bg-blue-700 p-1">
+    <button
+      v-for="tab in tabs"
+      :key="tab.id"
+      @click="$emit('change', tab.id)"
+      :class="[
+        'flex-1 rounded-lg px-3 py-2 text-sm font-semibold text-white transition',
+        active === tab.id ? 'bg-blue-900' : 'hover:bg-blue-800'
+      ]"
+    >
+      {{ tab.label }}
+    </button>
+  </div>
+</template>
